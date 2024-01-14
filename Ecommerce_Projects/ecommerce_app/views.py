2ec775f4f5aeb1c2a56919f680b34b94c64a8578
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-14 12:42:05 +0545

    added views and urls

--- Ecommerce_Projects/ecommerce_app/views.py
+++ Ecommerce_Projects/ecommerce_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class ProductView(generics.ListCreateAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializers
+
+    def create(self, request, *args, **kwargs):
+        serializers=ProductSerializers(many=True,data=request.data)
+        serializers.is_valid()
+        serializers.save
+        return Response(serializers.data)
