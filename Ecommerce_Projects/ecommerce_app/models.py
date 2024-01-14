9606a617ac9ad4098586d1653e490e51a092a6e2
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-14 12:35:45 +0545

    added models

--- Ecommerce_Projects/ecommerce_app/models.py
+++ Ecommerce_Projects/ecommerce_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=20)
+    price=models.IntegerField()
+    descriptions=models.CharField(max_length=50)
+    # image=models.ImageField(default='img.png')
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    item=models.CharField(max_length=30)
+    descriptions=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.item
+    
+
+class ProductCategory(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
