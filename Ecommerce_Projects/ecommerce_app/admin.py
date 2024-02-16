5838e4c328abe6a456590ec62b276762328be265
Dhiraj Sapkota <101037569+Dheeruvaii@users.noreply.github.com>
2024-02-16 11:12:17 +0000

    register models in admin pannel

--- Ecommerce_Projects/ecommerce_app/admin.py
+++ Ecommerce_Projects/ecommerce_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import User, Product, Category, ProductCategory, Cart
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'descriptions', 'quantity', 'is_available', 'created_at', 'modified_at')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('item',)
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    pass  # No special configuration needed for this model
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'created_by', 'created_at', 'updated_at')
